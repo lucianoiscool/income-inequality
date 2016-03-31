88496b48ff407d1bb276c301fd5ff59ad158008b
Luciano <lucianoarango@college.harvard.edu>
2016-03-30 21:39:22 -0400

    seperate projects, start organizing data

--- js/main.js
+++ js/main.js
@@ -16,20 +16,20 @@
 
     /* Show alert when leaving a .gov domain. */
     function alertLeavingUsg(e, newHostname) {
-      if (!newHostname.match(/\.gov$/)) {
-        if (confirm('You are about to leave this web site for a destination ' +
-                    'outside of the Federal Government. You may wish to ' +
-                    'review each privacy notice since their information ' +
-                    'collection practices may differ from ours. In addition, ' +
-                    'our linking to these sites does not constitute an ' +
-                    'endorsement of any products or services.\n\n' +
-                    'Click OK if you wish to continue to the web site, ' +
-                    'otherwise click Cancel to return to our site.')) {
-          // consinue
-        } else {
-          e.preventDefault();
-        }
-      }
+      //if (!newHostname.match(/\.gov$/)) {
+      //  if (confirm('You are about to leave this web site for a destination ' +
+      //              'outside of the Federal Government. You may wish to ' +
+      //              'review each privacy notice since their information ' +
+      //              'collection practices may differ from ours. In addition, ' +
+      //              'our linking to these sites does not constitute an ' +
+      //              'endorsement of any products or services.\n\n' +
+      //              'Click OK if you wish to continue to the web site, ' +
+      //              'otherwise click Cancel to return to our site.')) {
+      //    // consinue
+      //  } else {
+      //    e.preventDefault();
+      //  }
+      //}
     }
 
     $('a').click(function(e) {
